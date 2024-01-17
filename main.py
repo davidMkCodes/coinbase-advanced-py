5e98dcbbf15459c702fb60b6f75f4598201ea42f
davidMkCodes <72777029+davidMkCodes@users.noreply.github.com>
2024-01-17 15:16:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from json import dumps
 from coinbase.rest import RESTClient
 
-api_key = "organizations/580a132a-4496-40c8-bda9-024a5a70d29c/apiKeys/0724df8b-744d-49c3-a85a-34562364b121"
-api_secret = "-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIBPTxO6CNbH0WmglA2ISiAmKp4P8snmBIi3gjiOd45IWoAoGCCqGSM49\nAwEHoUQDQgAEXsJkko1qK5ljuLcF6t1jci7TIJRtGH6YnkCKgXxPXL+ReOw9FfNM\nexdWhv3BXcwpr1tomazbgHo+sGRLmwp1Wg==\n-----END EC PRIVATE KEY-----\n"
+api_key = ""
+api_secret = ""
 
 client = RESTClient(api_key=api_key, api_secret=api_secret)
 
